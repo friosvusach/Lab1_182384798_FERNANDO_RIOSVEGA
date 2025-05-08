a23a71519c7a2ede246646be240764e77c93f733
unknown <fernando.rios@orionsoluciones.cl>
2025-05-08 00:13:37 -0400

    Acomoda archivo main para que importe correctamente los archivos .rkt

---
+++ main_182384798_FERNANDO_RIOSVEGA.rkt
@@ -0,0 +1,7 @@
+#lang racket
+(require "tda_jugador_182384798_FERNANDO_RIOSVEGA.rkt")
+(require "tda_propiedad_182384798_FERNANDO_RIOSVEGA.rkt")
+(require "tda_tablero_182384798_FERNANDO_RIOSVEGA.rkt")
+(require "tda_carta_182384798_FERNANDO_RIOSVEGA.rkt")
+(require "tda_juego_182384798_FERNANDO_RIOSVEGA.rkt")
+(require "tda_utiles_182384798_FERNANDO_RIOSVEGA.rkt")
